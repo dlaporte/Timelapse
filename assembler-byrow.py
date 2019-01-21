bb69adeaef027b3baf2afb3c118675aebc135aa9
David LaPorte <david@davidlaporte.org>
2019-01-21 09:05:46 -0500

    updated -byrow and -bycol

--- assembler-byrow.py
+++ assembler-byrow.py
@@ -1,5 +1,16 @@
 #!/usr/local/bin/python3
 
+# join the output of this script into rows using:
+#
+# montage 101*.jpg -mode concatenate -tile 44 cabin1.png
+# montage 102*.jpg -mode concatenate -tile 44 cabin2.png
+# etc...
+#
+# and then join those using:
+#
+# montage cabin*.png -mode concatenate -tile 1 cabin.png
+#
+
 import ephem
 from datetime import datetime
 from datetime import timedelta
@@ -14,10 +25,10 @@ timelapse_directory = '/Users/dlaporte/Dropbox/test'
 timelapse_prefix = 'cabin-timelapse-'
 #timelapse_suffix = 'tiff'
 timelapse_suffix = 'jpg'
-final_filename = 'cabin_montage.jpg'
+final_directory = '/Users/dlaporte/Dropbox/Code/Timelapse/test-byrow'
 
-timelapse_start = '01/18/19'
-timelapse_end = '01/19/19'
+timelapse_start = '01/17/19'
+timelapse_end = '01/20/19'
 
 start = datetime.strptime(timelapse_start, '%m/%d/%y')
 end = datetime.strptime(timelapse_end, '%m/%d/%y')
@@ -41,9 +52,6 @@ while (day <= end):
 					if target_height is None:
 						target_height = img.height
 						target_width = img.width
-						with Image(width=target_width, height=target_height) as final_image:
-							img.save(filename=final_filename)
-						#print(str(target_width) + ',' + str(target_height))
 					else:
 						if target_height != img.height:
 							exit(day.strftime('%Y-%m-%d') + ': mis-matched resolution (' + str(img.height) + ')')
@@ -76,14 +84,12 @@ img_num = 100
 for day in days:
 	day_num += 1
 	for img in days[day]:
+		img.rotate(1.7)
 		img.crop(x, y, width=column_width, height=row_height)
-		#print(img.size)
 		file = str(day_num) + '_' + str(img_num) + '.jpg'
-		img.save(filename=file)
+		img.save(filename=final_directory + '/' + file)
 		img_num += 1
 		x += column_width
-
 	y += row_height
 	x = 0
-
 exit()

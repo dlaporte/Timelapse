eff8c08ce4c623823a3417b5ed380cada54260da
root <root@timelapse>
2019-05-17 10:27:40 -0400

    added subdirectories

--- snapshot.py
+++ snapshot.py
@@ -7,10 +7,15 @@ file_prefix = "cabin-timelapse-"
 pktriggercord_cli = "/usr/bin/pktriggercord-cli --model K-5 --file_format dng -m AV -i 100 -a 11 --ae_metering="
 save_location = "/root/cabin-timelapse"
 dropbox_uploader = "/root/bin/dropbox_uploader.sh"
+
 # dropbox location is relative to "/Apps/Cabin Timelapse"
-dropbox_location = "/"
+dropbox_subdir = datetime.now().strftime("%Y-%m-%d")
+
+dropbox_location = dropbox_subdir
+
+# create directory for snapshots
+os.system(dropbox_uploader + " mkdir " + dropbox_location)
 
 filename = save_location + "/" + file_prefix + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + ".dng"
 os.system(pktriggercord_cli + " > " + filename)
 os.system(dropbox_uploader + " upload " + filename + " " + dropbox_location + " && rm -f " + filename)
-
